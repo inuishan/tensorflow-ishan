252d2c966be427e62567162e3c5ce05951db1a9a
Ishan <ijain@sprinklr.com>
2016-11-28 15:24:17 +0530

    multiplication effect

--- softmax.py
+++ softmax.py
@@ -3,16 +3,17 @@ __time__ = '28/11/16'
 
 """Softmax."""
 
-scores = [[3.0, 1.0, 0.2], [3.0, 1.0, 0.2]]
+scores = [[30, 10, 02], [.30, .10, .02]]
 
 import numpy as np
+
 
 def softmax(x):
 	"""Compute softmax values for each sets of scores in x."""
 	y = np.exp(x)
-	print(y)
+	# print(y)
 	z = np.divide(y, np.sum(y, axis=0))
-	print(z)
+	# print(z)
 	return z
 
 
@@ -20,6 +21,7 @@ print(softmax(scores))
 
 # Plot softmax curves
 import matplotlib.pyplot as plt
+
 x = np.arange(-2.0, 6.0, 0.1)
 scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
 
